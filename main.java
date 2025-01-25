ae7160e91bf7f6a7298948b3e4cacf911f950471
Madhesh S <madheshit2027@gmail.com>
2025-01-25 19:46:58 +0530

    Add files via upload
    My first program in java program of getting input and print output from user

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.print("Enter any string:");
+        Scanner in = new Scanner(System.in);
+        String ch = in.next();
+        System.out.println(ch);
+    }
+}
